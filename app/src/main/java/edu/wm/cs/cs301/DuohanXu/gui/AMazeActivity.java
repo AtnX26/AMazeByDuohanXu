e458e738aaf9d7d966410349e70d6d55defd0d05
Duohan Xu <dxu03@wm.edu>
2022-11-21 12:00:11 -0500

    Set layout of AMazeActivity and implement part of code to support its functions

--- app/src/main/java/edu/wm/cs/cs301/DuohanXu/gui/AMazeActivity.java
+++ app/src/main/java/edu/wm/cs/cs301/DuohanXu/gui/AMazeActivity.java
@@ -2,18 +2,53 @@ package edu.wm.cs.cs301.DuohanXu.gui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.SeekBar;
 
 import edu.wm.cs.cs301.DuohanXu.R;
 
 public class AMazeActivity extends AppCompatActivity {
+    private Intent intent;
+
+    private int skilllevel;
+    private int seed;
+    private String tag = "AMazeActivity";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_amaze);
+        Log.v(tag,"created");
 
-        SeekBar seekbar = (SeekBar) findViewById(R.id.seekbar);
+        //Control for the seekbar to adjust complexity
+        SeekBar seekbar = findViewById(R.id.complexity);
+        seekbar.setMax(9);
+        seekbar.setProgress(0);
+        int seekBarValue= seekbar.getProgress();
+        seekbar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            int passedValue = 0;
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                passedValue = progress;
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                Log.v(tag, "Maze level is :" + passedValue);
+
+            }
+        });
+
+
     }
+
+
 }
